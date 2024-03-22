ed2b5c117dfe1bbb9e092319391ae6f364f79ce6
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-03-22 11:09:32 +0600

    Building custom interceptor.

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,3 @@
-import { Exclude } from 'class-transformer';
 import { AfterRemove, AfterInsert, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -10,7 +9,6 @@ export class User {
     email: string;
 
     @Column()
-    @Exclude()
     password: string;
 
     @AfterInsert()
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundExce
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -14,9 +15,10 @@ export class UsersController {
         return this.userService.create(email, password);
     }
 
-    @UseInterceptors(ClassSerializerInterceptor)
+    @UseInterceptors(SerializeInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
+        console.log('Handler is running');
         const user = await this.userService.findOne(parseInt(id));
         if (!user) {
             throw new NotFoundException('User not found');

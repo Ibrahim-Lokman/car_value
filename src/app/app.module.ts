8b5f07bd4632c9d8fc8e9a61cfde24321e92f008
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-03-22 11:49:09 +0600

    Minor Restructure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from '../users/users.module';
+import { ReportsModule } from '../reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
+import { User } from '../users/user.entity';
+import { Report } from '../reports/report.entity';
 
 @Module({
   imports: [
